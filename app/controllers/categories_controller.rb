88282ade1ded9ff9c1dd0b17334ad786c5b32b9d
lemonjai <leocheung_1986@hotmail.com>
2016-03-29 20:33:09 -0400

    added carrier wave to upload images

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,29 @@
 class CategoriesController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = "Category Saved"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
 end
